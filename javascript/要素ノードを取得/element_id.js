19746e917aa088c447cf042d2ac3aaafaf5eced3
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-05-25 17:35:44 +0900

    Add files via upload

---
+++ javascript/要素ノードを取得/element_id.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var current = new Date();
+var result = document.getElementById('result');
+result.textContent = current.toLocaleString();
---
+++ javascript/要素ノードを取得/element_tag.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// ページ内のすべてのアンカータグを取得
+//var list = document.getElementsByTagName('a');
+var list = document.getElementsByTagName('a');
+// リスト(HTMLCollectionオブジェクト)から順にアンカータグを取り出し、そのhref属性をログに出力
+/*for (var i = 0, len = list.length; i < len; i++) {
+    console.log(list.item(i).href);
+}*/
+for (var i = 0, len = list.length; i < len; i++) {
+    console.log(list.item(i).href);
+}
