9b1eca58d31078b98d0d800a712fe4fd217c82cd
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-22 04:49:59 +0900

    change

---
+++ javascript/タイマー機能を実装する/interval_async.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function hoge() {
+    console.log('‚ ‚¢‚¤‚¦‚¨');
+    setTimeout(function () {
+        console.log('‚©‚«‚­‚¯‚±');
+    }, 0);
+    console.log('‚³‚µ‚·‚¹‚»');
+}
+
+hoge();
