56e893337b205df49202c24bf5aade68ba44ae7b
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-22 04:25:47 +0900

    change

---
+++ javascript/タイマー機能を実装する/interval.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // タイマー設置
+    var timer = window.setInterval(
+        // 現在の時刻を<div id='result'>要素に表示（5000ミリ秒ごとに更新）
+        function () {
+            var dat = new Date();
+            document.getElementById('result').textContent = dat.toLocaleTimeString();
+        }, 5000);
+
+    // ボタンクリック時にタイマー処理を中止
+    document.getElementById('btn').addEventListener('click', function () {
+        window.clearInterval(timer);
+    }, false);
+}, false);
