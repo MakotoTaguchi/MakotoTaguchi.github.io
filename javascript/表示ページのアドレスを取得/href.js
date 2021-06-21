c13d8048e13e69154ee59060d96469592a65833d
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-22 05:09:44 +0900

    change

---
+++ javascript/表示ページのアドレスを取得/href.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('isbn').addEventListener('change', function () {
+        location.href = 'http://www.wings.msn.to/index.php/-/A-03/' + this.value;
+    }, false);
+}, false);
