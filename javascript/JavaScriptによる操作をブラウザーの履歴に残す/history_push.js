f8a56e8873513928d7fd35abe870bb6dfa1d8891
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-22 21:58:32 +0900

    change

--- javascript/JavaScriptによる操作をブラウザーの履歴に残す/history_push.js
+++ javascript/JavaScriptによる操作をブラウザーの履歴に残す/history_push.js
@@ -5,7 +5,7 @@ var result = document.getElementById('result');
 // [カウントアップ]ボタンをクリックした時に履歴を追加
 document.getElementById('btn').addEventListener('click', function () {
     result.textContent = ++count;
-    history.pushState(count, null, '/javascript/JavaScriptによる操作をブラウザーの履歴に残す/count' + count);
+    history.pushState(count, null, '/MakotoTaguchi.github.io/javascript/JavaScriptによる操作をブラウザーの履歴に残す/history_push.html');
 });
 
 // [戻る]ボタンでページの状態を前に戻す
