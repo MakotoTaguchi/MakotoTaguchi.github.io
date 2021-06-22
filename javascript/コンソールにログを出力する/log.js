68089e60e4207c860b11fda5bce6b658861a954a
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-23 00:15:43 +0900

    change

---
+++ javascript/コンソールにログを出力する/log.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log('ƒƒO');
+console.info('î•ñ');
+console.warn('Œx');
+console.error('ƒGƒ‰[');
---
+++ javascript/知っておくと便利なログメソッド/log_count.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.count('LOOP');
+    }
+}
+console.count('LOOP');
