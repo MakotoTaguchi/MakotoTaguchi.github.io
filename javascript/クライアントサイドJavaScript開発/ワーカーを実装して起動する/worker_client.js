5c6556fc6faf5ae535fef9b14be2cff082e9dc2e
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-24 18:29:05 +0900

    change

---
+++ javascript/クライアントサイドJavaScript開発/ワーカーを実装して起動する/worker_client.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn').addEventListener('click', function () {
+        var worker = new Worker('worker.js');
+
+        worker.postMessage({
+            'target': document.getElementById('target').value,
+            'x': document.getElementById('x').value
+        });
+        document.getElementById('result').textContent = 'ŒvŽZ’†...';
+
+        worker.addEventListener('message', function (e) {
+            document.getElementById('result').textContent = e.data;
+        }, false);
+
+        worker.addEventListener('error', function (e) {
+            document.getElementById('result').textContent = e.message;
+        }, false);
+    }, false);
+}, false);
