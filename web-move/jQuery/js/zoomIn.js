0753f1c448c6bacc1b58fb10b1cd5db1dd42c439
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-06-27 16:01:32 +0900

    change

---
+++ web-move/jQuery/js/zoomIn.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 要素やクラス名をクリックしたら動かす場合の記述
+$('.zoomInClickTrigger').on('click', function () {
+    $(this).addClass('zoomIn'); // 要素をクリックしたらzoomInというクラス名を追記
+})
+// ここまで要素やクラス名をクリックしたら動かす場合の記述
+
+// 要素やクラス名をクリックしたら何度も動かす場合の記述
+$('.zoomInClickTriggerTimes').on('click', function () {
+    $(this).addClass('zoomIn'); // 要素をクリックしたらzoomInというクラス名を追記
+    $(".zoomIn").on('animationend', function () {
+        $(this).removeClass('zoomIn'); // アニメーションが終了したらクラス名を取り除く
+    });
+});
+// ここまで要素やクラス名をクリックしたら動かす場合の記述
