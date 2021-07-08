5319f1b6803e0f42aaadf35336d3da1b846a50ce
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-07-08 09:47:59 +0900

    change

---
+++ web-move/jQuery/transition/js/extend-updown.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(window).on('load', function () {
+    //ロゴを1.2秒でフェードアウトする記述
+    $("#splash-logo").delay(1200).fadeOut('slow')
+    //ローディングエリア(splash)エリアを1.5秒でフェードアウトする記述
+    $("#splash").delay(1500).fadeOut('slow', function () {
+        $('body').addClass('appear'); //フェードアウト後bodyにappearクラス付与    
+    });
+    $('.splashbg1').on('animationend', function () {
+        //背景が伸びた後に動かしたいJavaScriptがある場合はこの中にJavaScriptを記載
+    });
+});
