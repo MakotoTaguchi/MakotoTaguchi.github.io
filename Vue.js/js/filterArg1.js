693d18a65df46f61a40383a5df2f7891b4023a67
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-08-23 17:52:45 +0900

    main

---
+++ Vue.js/js/filterArg1.js
@@ -0,0 +1,13 @@
+Vue.filter('addComma', function (num, end) {
+    if (end) {
+        return num.toLocaleString() + end;
+    } else {
+        return num.toLocaleString();
+    }
+});
+var app = new Vue({
+    el: '#app',
+    data: {
+        num: 10000,
+    },
+});
