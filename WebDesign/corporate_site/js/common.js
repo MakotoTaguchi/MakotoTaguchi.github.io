c6717d26ab2bbad661cef1713460b750792fa504
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-07 23:29:26 +0900

    main

--- WebDesign/corporate_site/js/common.js
+++ WebDesign/corporate_site/js/common.js
@@ -1,4 +1,15 @@
 $(function () {
     let topBtn = $("#scroll-top");
     topBtn.hide();
+
+    $(window).scroll(function () {
+        console.log($(this).scrollTop());
+        console.log($(this).height());
+
+        if ($(this).scrollTop() > 1000) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
 })
