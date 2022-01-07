c3e1ea6b78ee46166e302fd4d426b55d1fce2f85
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-07 23:35:54 +0900

    main

--- WebDesign/corporate_site/js/common.js
+++ WebDesign/corporate_site/js/common.js
@@ -3,13 +3,21 @@ $(function () {
     topBtn.hide();
 
     $(window).scroll(function () {
-        console.log($(this).scrollTop());
-        console.log($(this).height());
+        // console.log($(this).scrollTop());
+        // console.log($(this).height());
 
         if ($(this).scrollTop() > 1000) {
             topBtn.fadeIn();
         } else {
             topBtn.fadeOut();
         }
+    });
+
+    topBtn.on("click", function (event) {
+        console.log(event);
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
     });
 })
