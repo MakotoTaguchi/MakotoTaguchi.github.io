9d10b243229a31d2496976127a9d31d284b6b05f
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-01-21 17:23:42 +0900

    main

--- WebDesign/restaurant_site/js/script.js
+++ WebDesign/restaurant_site/js/script.js
@@ -1,3 +1,21 @@
 $(".openbtn").click(function () {
     $(this).toggleClass('active');
 });
+
+$('.slider').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+});
+
+$('.slider').on('touchmove', function (event, slick, currentSlide, nextSlide) {
+    $('.slider').slick('slickPlay');
+});
